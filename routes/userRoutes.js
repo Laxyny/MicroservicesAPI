1f158d95d154b374019df97f0f70004d180e497a
Théo MIRANDA <theo.miranda34@gmail.com>
2024-12-03 16:49:49 +0100

    *ADD* Refuser la création d'un magasin pour un non-vendeur

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const storeController = require('../controllers/storeController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
-//Routes Users
+// Routes Users
 router.get('/users', userController.getAllUsers);
 router.get('/user/:id', userController.getUser);
 router.post('/register', userController.createUser);
@@ -12,10 +13,10 @@ router.post('/login', userController.loginUser);
 router.put('/updateUser/:id', userController.updateUser);
 router.delete('/deleteUser/:id', userController.deleteUser);
 
-//Routes Stores
+// Routes Stores
 router.get('/stores', storeController.getAllStores);
 router.get('/store/:id', storeController.getStore);
-router.post('/createStore', storeController.createStore);
+router.post('/createStore', authMiddleware("seller"), storeController.createStore); // Seuls les sellers peuvent créer un magasin
 
 router.put('/updateStore/:id', storeController.updateStore);
 router.delete('/deleteStore/:id', storeController.deleteStore);
