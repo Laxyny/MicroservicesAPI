2d8ed44f21cdce6b4dfdd0ca698e157e5c3c769e
Laxyny <kevgreg30@gmail.com>
2024-12-03 16:10:42 +0100

    *ADD* Homepage avec role + nom + deconnexion & sauvegardes cookies

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const storeController = require('../controllers/storeController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 //Routes Users
 router.get('/users', userController.getAllUsers);
+router.get('/user', authMiddleware, userController.getUserFromToken);
 router.get('/user/:id', userController.getUser);
 router.post('/register', userController.createUser);
 router.post('/login', userController.loginUser);
+router.post('/logout', userController.logout);
 
 router.put('/updateUser/:id', userController.updateUser);
 router.delete('/deleteUser/:id', userController.deleteUser);
