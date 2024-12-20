c634bb2a6d4ea10f57a0fd93fed6652f2d7ac36b
Laxyny <kevgreg30@gmail.com>
2024-12-20 16:26:16 +0100

    **FIX** Ajout du cors

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const cors = require(
+    'cors'
+)
 const app = express();
 const port = 3000;
 
@@ -27,6 +30,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(express.static('public'));
+app.use(cors());
 
 async function run() {
     try {
